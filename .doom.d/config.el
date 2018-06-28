ae63fb0b66726a4525e0430a9c2dbc79dd873a4c
oisincar <oisincar@gmail.com>
2018-06-28 21:50:30 +0100

    Add C# binds, fix ag search on windows.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,3 +3,9 @@
 (load! "bindings")
 ;(load! bindings)
 (setq doom-font (font-spec :family "Source Code Pro" :size 18))
+
+;; Make search work using silver-searcher on windows.
+;; (Not updating value for some reason??)
+(setq counsel-ag-base-command "ag -zS --vimgrep --nocolor --nogroup %s")
+
+;; (setq visual-line-mode t)
